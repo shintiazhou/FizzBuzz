a088355956fb754895036e0a69b741f3347a837f
shintia <shintiazhouu28@gmail.com>
2022-11-10 11:02:54 +0700

    fix layout

--- FizzBuzz tugas 1/Pelanggan.Designer.cs
+++ FizzBuzz tugas 1/Pelanggan.Designer.cs
@@ -140,7 +140,7 @@ namespace FizzBuzz_tugas_1
             this.tabControl.Controls.Add(this.tabPembayaran);
             this.tabControl.Controls.Add(this.tabRiwayat);
             this.tabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabControl.Location = new System.Drawing.Point(213, 44);
+            this.tabControl.Location = new System.Drawing.Point(213, -25);
             this.tabControl.Name = "tabControl";
             this.tabControl.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.tabControl.SelectedIndex = 0;
@@ -1061,7 +1061,7 @@ namespace FizzBuzz_tugas_1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 644);
+            this.ClientSize = new System.Drawing.Size(808, 644);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.btnAkun);
             this.Controls.Add(this.btnKeluar);
